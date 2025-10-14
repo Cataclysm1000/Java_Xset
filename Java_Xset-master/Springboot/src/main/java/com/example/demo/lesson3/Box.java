8c0d705331eea770d91085978f682845c3ae814b
Cataclysm1000 <Cataclysm100000@gmail.com>
2025-10-14 20:47:07 +0500

    Add files via upload

---
+++ Java_Xset-master/Springboot/src/main/java/com/example/demo/lesson3/Box.java
@@ -0,0 +1,17 @@
+package com.example.demo.lesson3;
+
+public interface Box {
+    void addSweet(Sweets sweet);
+
+    void removeLast();
+
+    double getTotalWeight();
+
+    double getTotalPrice();
+
+    void printAll();
+
+    double optimizeWeight(double maxWeight);
+
+    double optimizePrice(double maxPrice);
+}
